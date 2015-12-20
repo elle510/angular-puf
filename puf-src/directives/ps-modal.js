53a5f3f787e47a0920c8892a2af99912014624e7
elle510 <elle0510@gmail.com>
2015-12-20 22:22:37 +0900

    modal 수정

--- puf-src/directives/ps-modal.js
+++ puf-src/directives/ps-modal.js
@@ -33,14 +33,14 @@ angular.module('ps.directives.modal', [])
 			 size: 		'@',
 			 backdrop: 	'@'
 		 },
-		 template: '<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">' +
+		 template: '<div class="modal fade" ng-class="className" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">' +
 		    			'<div class="modal-dialog" ng-class="{\'modal-sm\': size == \'sm\', \'modal-lg\': size == \'lg\'}" ng-style="{width: width}">' +
 		    				'<div class="modal-content" ps-modal-transclude>' +
 		    				'</div>' +
 		    			'</div>' +
 		    		'</div>',
 		 link: function (scope, element, attrs) {
-			 element.addClass(attrs.className || '');
+//			 element.addClass(attrs.className || '');
 		     scope.size = attrs.size;
 		     
 		     if(attrs.backdrop == 'static') {
@@ -50,8 +50,57 @@ angular.module('ps.directives.modal', [])
 		 }
 	 };
 })
+.directive('psModalHeader', ['$compile', function($compile) {
+	return {
+		require: '^psModal',
+		restrict: 'EA',
+		transclude: true,
+		replace: true,
+		scope: {
+			title: '@'
+		},
+		template: '<div class="modal-header">' +
+					'<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+					'<span class="modal-title">{{title}}</span>' +
+				  '</div>',
+		link: function(scope, element, attrs, ctrl) {
+			
+		}
+	}
+}])
+.directive('psModalBody', ['$compile', function($compile) {
+	return {
+		require: '^psModal',
+		restrict: 'EA',
+		transclude: true,
+		replace: true,
+		scope: {
+			
+		},
+		template: '<div class="modal-body" ng-transclude></div>',
+		link: function(scope, element, attrs, ctrl) {
+			
+		}
+	}
+}])
+.directive('psModalFooter', ['$compile', function($compile) {
+	return {
+		require: '^psModal',
+		restrict: 'EA',
+		transclude: true,
+		replace: true,
+		scope: {
+			
+		},
+		template: '<div class="modal-footer" ng-transclude></div>',
+		link: function(scope, element, attrs, ctrl) {
+			
+		}
+	}
+}])
 .directive('psModalTransclude', function() {
 	return {
+		restrict: 'A',
 		link: function(scope, element, attrs, controller, transclude) {
 	        transclude(scope.$parent, function(clone) {
 	          element.empty();
--- webapps/puf-app/scripts/controllers/components/elements/modalCtrl.js
+++ webapps/puf-app/scripts/controllers/components/elements/modalCtrl.js
@@ -4,11 +4,17 @@ define(['app'], function(app) {
 	
 	var controller = function ($scope, $location) {
         
-		$scope.pageTitle = "Modal";
+		$scope.pageTitle = 'Modal';
 		
 		$scope.prettyPrint = function() {
 			prettyPrint();
-		}
+		};
+		
+		$scope.content = '내용';
+		
+		$scope.smClick = function() {
+			$scope.content = '내용123';
+		};
     };
     
 	app.register.controller('modalCtrl', ['$scope', '$location', controller]);
